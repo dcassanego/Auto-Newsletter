141b06abfcd81899fc12478fc178dd01189fbcb3
Dante Cassanego <dante.cassanego@gmail.com>
2011-04-17 17:56:21 -0400

    host name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,4 +47,9 @@ AutoNewsletter::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+  
+  config.action_mailer.default_url_options = {
+  :host => "sloan-newsletters.heroku.com",
+  :port => 80
+}
 end
