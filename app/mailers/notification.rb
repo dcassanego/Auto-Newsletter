d9755feb639fd6e43c9f460172f64dc7a7f24954
Dante Cassanego <dante.cassanego@gmail.com>
2012-05-16 17:13:21 -0400

    again

--- app/mailers/notification.rb
+++ app/mailers/notification.rb
@@ -2,8 +2,8 @@ class Notification < ActionMailer::Base
   default :from => "mitsloannetimpactmarketing@gmail.com"
   
   def send_reminder(newsletter)
-    ActionMailer::Base.smtp_settings.user_name = newsletter.from_email
-    ActionMailer::Base.smtp_settings.password = newsletter.from_pwd
+    ActionMailer::Base.smtp_settings[:user_name] = newsletter.from_email
+    ActionMailer::Base.smtp_settings[:password] = newsletter.from_pwd
     
     @newsletter = newsletter
     mail( :to => newsletter.moderators.collect {|m| m.email},
@@ -12,8 +12,8 @@ class Notification < ActionMailer::Base
   end
   
   def event_added(announcement)
-    ActionMailer::Base.smtp_settings.user_name = announcement.newsletter.from_email
-    ActionMailer::Base.smtp_settings.password = announcement.newsletter.from_pwd
+    ActionMailer::Base.smtp_settings[:user_name] = announcement.newsletter.from_email
+    ActionMailer::Base.smtp_settings[:password] = announcement.newsletter.from_pwd
     
     @announcement = announcement
     mail( :to => announcement.newsletter.moderators.collect {|m| m.email},
--- app/mailers/weekly_newsletter.rb
+++ app/mailers/weekly_newsletter.rb
@@ -2,8 +2,8 @@ class WeeklyNewsletter < ActionMailer::Base
   default :from => "mitsloannetimpactmarketing@gmail.com"
   
   def newsletter(newsletter)
-    ActionMailer::Base.smtp_settings.user_name = newsletter.from_email
-    ActionMailer::Base.smtp_settings.password = newsletter.from_pwd
+    ActionMailer::Base.smtp_settings[:user_name] = newsletter.from_email
+    ActionMailer::Base.smtp_settings[:password] = newsletter.from_pwd
     
     @newsletter = newsletter
     @announcements = Announcement.upcoming(@newsletter)    
