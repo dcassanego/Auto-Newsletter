91fe0a6c4ed283a11a52dc045811a58f61a5ec1d
Dante Cassanego <dante.cassanego@gmail.com>
2012-08-29 07:53:39 -0400

    whoops

--- config/initializers/exception_notification.rb
+++ config/initializers/exception_notification.rb
@@ -1,4 +1,4 @@
-Gradeables::Application.config.middleware.use ExceptionNotifier,
+AutoNewsletter::Application.config.middleware.use ExceptionNotifier,
   :email_prefix => "[Exception] ",
   :sender_address => %{"Auto Newsletter" <mitsloannetimpactmarketing@gmail.com>},
   :exception_recipients => %w{dante.cassanego@gmail.com}
